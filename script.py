f419505adb82788f865da641bf3b8b31ae1db8ce
Ankit Jindal <jindalankit940@gmail.com>
2025-07-17 20:36:24 +0530

    Added code

--- script.py
+++ script.py
@@ -3,9 +3,12 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Automatically loads .env file
 
 def send_email(workflow_name, repo_name, workflow_run_id):
-    sender_emsil = os.getenv('SENDER_EMAIL')
+    sender_email = os.getenv('SENDER_EMAIL')
     sender_password = os.getenv('SENDER_PASSWORD')
     reciver_email = os.getenv('RECIVER_EMAIL')
 
@@ -13,7 +16,7 @@ def send_email(workflow_name, repo_name, workflow_run_id):
     body = f"Hi, the workflow {workflow_name} failed for the repo {repo_name}. Please check logs for more details. \nMore Details: \nRun_ID: {workflow_run_id}."
 
     msg = MIMEMultipart()
-    msg['From'] = sender_emsil
+    msg['From'] = sender_email
     msg['To'] = reciver_email
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
@@ -21,14 +24,15 @@ def send_email(workflow_name, repo_name, workflow_run_id):
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login(sender_emsil, sender_password)
+        server.login(sender_email, sender_password)
         text = msg.as_string()
-        server.sendmail(sender_emsil, reciver_email, text)
+        server.sendmail(sender_email, reciver_email, text)
         server.quit()
 
         print("Email sent successfully")
     except Exception as e:
         print(f"Error: {e}")
+
 
 
 send_email(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'))
